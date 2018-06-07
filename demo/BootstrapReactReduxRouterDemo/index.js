2c52c84df15bffb7aaa7182700eb79c07d8f1213
user <user@userdeMacBook-Pro.local>
2018-06-07 14:10:30 +0800

    Site updated: 2018-06-07 14:10:29

---
+++ demo/BootstrapReactReduxRouterDemo/index.js
@@ -0,0 +1,6 @@
+var ReactDOM =require('./build/react-dom');
+console.log("--------","----------")
+// ReactDOM.render(
+//     <h1>sdfdsfsddf</h1>,
+//     document.getElementById('root')
+// );
---
+++ demo/BootstrapReactReduxRouterDemo/main/MainPage.js
@@ -0,0 +1,40 @@
+/**
+ *  功能名称：
+ *  功能描述：
+ *  创建人：ronnie
+ *  创建时间：2018-03-28 17:51
+ *  创建人邮箱
+ *  修改人：
+ *  修改人邮箱：
+ *  修改时间：
+ *  修改原因：
+ */
+import React, { Component } from 'react';
+import 'main.css';
+
+export default  class MainPage extends Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            //body
+            <div className='bodyCotainer'>
+                {/*header*/}
+                <div>asdfsadfsadfsad</div>
+                {/*container*/}
+                <div>sadfsdafsdafdsafsad</div>
+                {/*leftContainer*/}
+                <div>sadfsadfsdafdsaf</div>
+                {/*rightContainer*/}
+                <div>sadfsadsdafsdadfsfdssdf</div>
+            </div>
+        )
+    }
+}
